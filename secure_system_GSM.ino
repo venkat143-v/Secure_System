ae935244a9f6ddcee015f5583ca2eeee46a61ee4
Sai Venkat Javvadi <81428675+venkat143-v@users.noreply.github.com>
2023-10-16 22:55:42 +0530

    Update secure_system_GSM.ino

--- secure_system_GSM.ino
+++ secure_system_GSM.ino
@@ -1,52 +1,56 @@
 #include <SoftwareSerial.h>
-const String PHONE = "+919392726667";
-#define rxPin 2
-#define txPin 3
-SoftwareSerial sim800(rxPin, txPin);
-#define trigPin 6
-#define echoPin 7
+
+#define PIR_PIN 2  // PIR sensor connected to digital pin 2
+SoftwareSerial gsmSerial(7, 8);  // RX, TX for GSM module
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  gsmSerial.begin(9600);
 
-  Serial.begin(115200);
+  pinMode(PIR_PIN, INPUT);
 
-  sim800.begin(9600);
-  Serial.println("SIM800L software serial initialize");
-
-  sim800.println("AT");
-  delay(1000);
+  // Initialize GSM module
+  initializeGSM();
 }
 
 void loop() {
+  // Read PIR sensor
+  int pirValue = digitalRead(PIR_PIN);
 
-  while (sim800.available()) {
-    Serial.println(sim800.readString());
+  if (pirValue == HIGH) {
+    Serial.println("Motion detected!");
+
+    // Send SMS
+    sendSMS("+1234567890", "Intruder detected!");
+
+    delay(10000);  // Wait for 10 seconds to avoid multiple alerts
   }
+}
 
-  while (Serial.available())  {
-    sim800.println(Serial.readString());
-  }
+void initializeGSM() {
+  delay(1000);
+  gsmSerial.println("AT");  // Check if the GSM module is responding
+  delay(1000);
 
-  long time_duration, distance_in_cm;
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  time_duration = pulseIn(echoPin, HIGH);
-  distance_in_cm = time_duration / 29 / 2;
+  gsmSerial.println("AT+CMGF=1");  // Set SMS mode to text
+  delay(1000);
 
-  Serial.print(distance_in_cm);
-  Serial.println(" Mm");
+  gsmSerial.println("AT+CNMI=2,2,0,0,0");  // New SMS message indications
+  delay(1000);
 
-  if (distance_in_cm <= 25) {
-    Serial.println("Water Level Increased");
-    Serial.println("calling....");
-    sim800.println("ATD" + PHONE + ";");
-    delay(1000); //20 sec delay
-  }
+  Serial.println("GSM module initialized");
+}
 
-  delay(500);
+void sendSMS(const char* phoneNumber, const char* message) {
+  gsmSerial.print("AT+CMGS=\"");
+  gsmSerial.print(phoneNumber);
+  gsmSerial.println("\"");
+
+  delay(1000);
+
+  gsmSerial.println(message);
+  gsmSerial.write(26);  // Ctrl+Z to end the SMS
+  delay(1000);
+
+  Serial.println("SMS sent");
 }
